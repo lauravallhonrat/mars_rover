9ee5afd63fbe8490e5d0e06fae991fa39be9487f
Laura Vallhonrat <vallhonratlaura@gmail.com>
2017-05-22 12:43:29 +0200

    movements, turn right and left functions

--- rover.js
+++ rover.js
@@ -2,10 +2,51 @@ var myRover = {
   position: [0, 0],
   direction: ['N', 'E', 'S', 'W'],
   currentPositon: 0,
-  stopRover: false,
-  obstacles: [[2, 3], [6, 7], [9, 10]]
+  stopRover: false
 }
 function getCommands (commands) {
   var instructions = commands.split(' ');
   return instructions;
+}
+
+function turnRoverRight(r){
+  if (rover.currentPositon +1 <= 3){
+      rover.currentPositon++;
+  } else {
+      rover.currentPositon = 0;
+  }
+}
+
+function turnRoverLeft(l){
+  if (rover.currentPositon -1 >= 1){
+      rover.currentPositon--;
+  } else {
+      rover.currentPositon = 0;
+  }
+}
+
+function roverMovements(instructions){
+  for (var i = 0; i < instructions.length; i++){
+
+  	if (rover.stopRover === false){
+
+    switch (instructions[i]){
+      case 'f':
+      moveRover('f');
+      break;
+
+      case 'b':
+      moveRover('b');
+      break;
+
+      case 'l':
+      turnRoverLeft();
+      break;
+
+      case 'r':
+      turnRoverRight();
+      break;
+    }
+  }
+}
 }
