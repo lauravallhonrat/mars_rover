4375bf2983bb3ea7f1b7d2f177198477ca248dfe
Laura Vallhonrat <vallhonratlaura@gmail.com>
2017-05-22 17:25:18 +0200

    changing logic, simplifying code, adding obstacles and adding html

--- rover.js
+++ rover.js
@@ -1,67 +1,111 @@
 var myRover = {
-  position: [0, 0],
-  direction: ['N', 'E', 'S', 'W'],
-  currentPositon: 0,
-  stopRover: false
-}
-function getCommands (commands) {
-  var instructions = commands.split(' ');
-  return instructions;
+  positionX : 0,
+  positionY : 0,
+  direction: 'N',
+  obstacle1X : 5,
+  obstacle1Y : 2,
+  obstacle2X : 3,
+  obstacle2Y : 7
 }
 
 function turnRoverRight(){
-  if (rover.currentPositon +1 <= 3){
-      rover.currentPositon++;
-  } else {
-      rover.currentPositon = 0;
-  }
+  switch(rover.direction)
+	{
+		case 'N':
+			myRover.direction = 'E';
+			break;
+		case 'S':
+			myRover.direction = 'W';
+			break;
+		case 'E':
+			myRover.direction = 'S';
+			break;
+		case 'W':
+			myRover.direction = 'N';
+			break;
+	}
 }
 
 function turnRoverLeft(){
-  if (rover.currentPositon -1 >= 1){
-      rover.currentPositon--;
-  } else {
-      rover.currentPositon = 0;
-  }
+  switch(myRover.direction)
+	{
+		case 'N':
+			myRover.direction = 'W';
+			break;
+		case 'S':
+			myRover.direction = 'E';
+			break;
+		case 'E':
+			myRover.direction = 'N';
+			break;
+		case 'W':
+			myRover.direction = 'S';
+			break;
+	}
 }
 
 function moveforward()
 {
-	switch(rover.direction)
+	switch(myRover.direction)
 	{
     case 'N':
-      rover.position[0]++
-      break;
-    case 'E':
-      rover.position[1]++
-      break;
-    case 'S':
-      rover.position[0]--
-      break;
-    case 'W':
-      rover.position[1]--
-      break;
-	}
-}
-function moveBackwards()
-{
-	switch(rover.direction)
-	{
-    case 'N':
-      rover.position[0]--
-      break;
-    case 'E':
-      rover.position[1]--
-      break;
-    case 'S':
-      rover.position[0]++
-      break;
-    case 'W':
-      rover.position[1]++
-      break;
+			myRover.positionY += 1;
+			break;
+		case 'S':
+			myRover.positionY -= 1;
+			break;
+		case 'E':
+			myRover.positionX += 1;
+			break;
+		case 'W':
+			myRover.positionX -= 1;
+			break;
 	}
 }
 
+function moveBackwards()
+{
+	switch(myRover.direction)
+	{
+    case 'N':
+			myRover.positionY -= 1;
+			break;
+		case 'S':
+			myRover.positionY += 1;
+			break;
+		case 'E':
+			myRover.positionX -= 1;
+			break;
+		case 'W':
+			myRover.positionX += 1;
+			break;
+	}
+}
+function gridLimits()
+{
+  if (positionX > 25) {
+    alert('You are out of limits!, setting to the default position...');
+    reset();
+  }
+  if (positionY > 25) {
+    alert('You are out of limits!, setting to the default position...');
+    reset();
+  }
+}
+function detectObstacle()
+{
+ if (positionX === obstacle1X && positionY === obstacle1Y) {
+   alert('your Rover has crashed!, setting to the default position...');
+   reset();
+ }
+ else if (positionX === obstacle2X && positionY === obstacle2Y) {
+   alert('your Rover has crashed!, setting to the default position...');
+   reset();
+ }
+ else {
+   writePosition();
+ }
+}
 function moveRover()
 {
 	var commands = document.getElementById("instruction").value;
@@ -70,20 +114,38 @@ function moveRover()
 	{
 		switch(commands[i])
 		{
-			case "f":
+			case 'f':
 	  			moveforward();
+          gridLimits();
 	  			break;
-			case "b":
+			case 'b':
 	 			  moveBackwards();
+          gridLimits();
 	  			break;
-	  	case "l":
+	  	case 'l':
 	 			  turnRoverLeft();
+          gridLimits();
 	  			break;
-			case "r":
+			case 'r':
 	 			  turnRoverRight();
+          gridLimits();
 	  			break;
 			default:
 	  			alert("Syntax error!\nCommand -" + commands[i] + "- unkwnown.");
 		}
 	}
+  writePosition();
+}
+
+function writePosition()
+{
+	document.getElementById('position').innerHTML = "<b>Rover Position: positionX=" + myRover.positionX + " positionY=" + myRover.positionY + " Facing " + myRover.direction + "</b>";
+}
+function reset()
+{
+	myRover.positionX = 0;
+	myRover.positionY = 0;
+	myRover.direction = 'N'
+	document.getElementById('instruction').value = "";
+	writePosition();
 }
