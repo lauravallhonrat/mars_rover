ede3fc826aaa6677fcdd712da534253215e70983
Laura Vallhonrat <vallhonratlaura@gmail.com>
2017-05-22 12:07:08 +0200

    setting rover

--- rover.js
+++ rover.js
@@ -1,25 +1,7 @@
 var myRover = {
-  position: [0,0], 
-  direction: 'N'
-};
-
-function goForward(rover) {
-  switch(rover.direction) {
-    case 'N':
-      rover.position[0]++
-      break;
-    case 'E':
-      rover.position[1]++
-      break;
-    case 'S':
-      rover.position[0]--
-      break;
-    case 'W':
-      rover.position[1]--
-      break;
-  };
-
-  console.log("New Rover Position: [" + rover.position[0] + ", " + rover.position[1] + "]")
+  position: [0, 0],
+  direction: ['N', 'E', 'S', 'W'],
+  currentPositon: 0,
+  stopRover: false,
+  obstacles: [[2, 3], [6, 7], [9, 10]]
 }
-
-goForward(myRover);
